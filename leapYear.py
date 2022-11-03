8d15e1d3aff0b1145f8d5f54af7b1bc0e06d84c9
ASWIN-P-INFOPRISM <83418991+ASWIN-P-INFOPRISM@users.noreply.github.com>
2022-11-03 11:24:48 +0530

    Add files via upload

---
+++ leapYear.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+start_year = date.today().year
+
+end_year = int(input("Enter the last year : "))
+
+print(f"Leap years between {start_year} and {end_year} : \n")
+
+for year in range(start_year,end_year) :
+
+    if (year % 4 == 0) and (year % 100 != 0) :
+
+        print(year,"\n")
+
+    elif (year % 400 == 0):
+
+        print(year,"\n")
