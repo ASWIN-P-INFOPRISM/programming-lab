71be27d8f172d58bf723757671b73d1ba8fe71d5
ASWIN-P-INFOPRISM <83418991+ASWIN-P-INFOPRISM@users.noreply.github.com>
2022-12-07 15:14:17 +0530

    Add files via upload

---
+++ cycle 2/no_of_chars_in_a_string.py
@@ -0,0 +1,20 @@
+text = input("Enter string : ")
+
+chars = list(text)
+
+print(chars)
+
+char_frequency = {}
+
+for char in chars :
+
+    if char in char_frequency :
+
+        char_frequency[char] += 1
+
+    else:
+
+        char_frequency[char] = 1
+
+print(char_frequency)
+     
